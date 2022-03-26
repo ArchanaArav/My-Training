48a545df1ab56b04986ac6b19929d0e24526b1bb
ArchanaArav <archuaravind01@gmail.com>
2022-03-26 08:25:49 +0400

    added a java file

---
+++ Add.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Add
+{
+public static void main(String args[])
+
+{
+Scanner reader = new Scanner(System.in);
+System.out.println("enter first no");
+int a= reader.nextInt();
+System.out.println("enter second no");
+int b= reader.nextInt();
+System.out.println("sum:" +(a+b));
+System.out.println("enter name");
+String name= reader.next();
+
+
+}
+
+
+
+}
